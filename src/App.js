b14aabc330895065dd5a07a008d34cef9e0145e5
Monalisa Onyango <+>
2024-08-08 13:43:11 +0300

    finished code along

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import { useState, useEffect } from "react";
-// Add useNavigate to import
-import { Outlet, useNavigate} from "react-router-dom";
+import { useState, useEffect} from "react";
+import { Outlet, Navigate, useNavigate} from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
-    // Add code to mock user authentication
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
 
-  const login = () =>{
+
+   const login = () =>{
     setIsLoggedIn(true);
-  }
+  };
 
   const logout = () =>{
     setIsLoggedIn(false);
   };
 
-    // Add programmatic navigation for login and logout
   useEffect(() =>{
     if (isLoggedIn) {
-        // navigates to Home route if user is logged in
       navigate("/");
     } else {
-        // navigates to Login route if user is logged out
       navigate("/login");
-    };
+    }
   }, [isLoggedIn]);
 
   return (
     <div className="app">
-      <NavBar logout={logout} />
-        { /*Pass login function to Outlet as context */}
+{/* Add conditional rendering so users have to be logged in to see pages on the site */}
+      {isLoggedIn ? <NavBar logout={logout}  /> : <Navigate to="/login" />}
       <Outlet context={login}/>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import { NavLink} from "react-router-dom";
-import "./NavBar.css";
-
-function NavBar() {
+import "./NavBar.css"
+function NavBar({ logout }) {
 
   return (
     <nav>
@@ -11,14 +10,19 @@ function NavBar() {
       >
         Home
       </NavLink>
+
       <NavLink
         to="/about"
         className="nav-link"
       >
         About
       </NavLink>
+
+      
+      {/* Add the logout function to handle the onClick event */}
+      <button onClick={logout}>Logout</button>
     </nav>
   );
-}
+};
 
 export default NavBar;
